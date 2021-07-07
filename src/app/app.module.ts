5ba8e62057079bbec7b2d28317f05411bfa29c49
dzan.operta <dzan.operta@tuwien.ac.at>
2021-07-07 12:05:43 +0200

    init internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,22 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AuthenticationModule} from './module/authentication/authentication.module';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
 import {AppErrorInterceptor} from './app-error.interceptor';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {InternationalizationModule} from './module/internationalization/internationalization.module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+
+/**
+ * The http loader factory : Loads the files from define path.
+ * @param {HttpClient} http
+ * @returns {TranslateHttpLoader}
+ * @constructor
+ */
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, '../assets/locales/', '.json');
+}
+
 
 @NgModule({
     declarations: [
@@ -16,14 +30,22 @@ import {AppErrorInterceptor} from './app-error.interceptor';
         BrowserModule,
         BrowserAnimationsModule,
         AppRoutingModule,
-        AuthenticationModule
+        AuthenticationModule,
+        InternationalizationModule.forRoot({ locale_id: 'en' }), // initiating with default language: en
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [
         {
             provide: HTTP_INTERCEPTORS,
             useClass: AppErrorInterceptor,
             multi: true
-        },
+        }
     ],
     bootstrap: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
